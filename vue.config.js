fd3d1a54c5cafb8b1057791acaacba16fd8ba3e0
codmic1330 <ch20026103@gmail.com>
2021-02-06 22:08:32 +0800

    no message

---
+++ vue.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    publicPath: process.env.NODE_ENV === 'production'
+      ? './'
+      : '/'
+  }
+  http://yangjuiyu.byethost12.com/StockAnalysis/index.php/bwi/getCommon
+  http://yangjuiyu.byethost12.com/StockAnalysis/index.php/test/bookin
